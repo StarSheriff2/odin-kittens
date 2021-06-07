721649f9989e51a5c180eb95cc4ac8d61d856852
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-06-06 22:52:19 -0600

    set controller and database model

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,44 @@
+class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      redirect_to root_url
+    else
+      render new, notice: 'this request was not successful'
+    end
+  end
+
+  def update
+    @kitten = kitten.find(params[:id])
+    @kitten.update(kitten_params)
+
+    if @kitten.save
+      redirect_to @kitten, notice: 'You have successfully updated this kitten\'s info.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @kitten = kitten.find(params[:id])
+    @kitten.destroy
+    redirect_to root_url, notice: 'Kitten record deleted.'
+  end
+
+  def kitten_params
+    params.require(:user).permit(:name, :age, :cuteness, :softness)
+  end
+end
