61eb6b0f095d31887e1ad40b7917b65ad29cda15
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-06-08 00:31:30 -0600

    fix ruby linter errors

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -4,7 +4,7 @@ class KittensController < ApplicationController
 
     respond_to do |format|
       format.html
-      format.json { render :json => @kittens }
+      format.json { render json: @kittens }
     end
   end
 
@@ -13,7 +13,7 @@ class KittensController < ApplicationController
 
     respond_to do |format|
       format.html
-      format.json { render :json => @kitten }
+      format.json { render json: @kitten }
     end
   end
 
--- test/models/kitten_test.rb
+++ test/models/kitten_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class KittenTest < ActiveSupport::TestCase
   # test "the truth" do
